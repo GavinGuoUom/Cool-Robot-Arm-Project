47cfc384e297e9d2684d21840aadd5b0f07d503c
GavinGuoUoM <jianwei.guo@postgrad.manchester.ac.uk>
2021-11-07 10:51:11 +0000

    gik

--- MATLAB/gik_temp.m
+++ MATLAB/gik_temp.m
@@ -1,19 +1,24 @@
+% Set Up the Robot Model
 lbr = importrobot('iiwa14.urdf'); % 14 kg payload version
 lbr.DataFormat = 'row';
 gripper = 'iiwa_link_ee_kuka';
-cupHeight = 0.2;
-cupRadius = 0.05;
+cupHeight = 0.2; %Define dimensions for the cup.
+cupRadius = 0.05; 
 cupPosition = [-0.5, 0.5, cupHeight/2];
-body = rigidBody('cupFrame');
+body = rigidBody('cupFrame'); %Add a fixed body to the robot model representing the center of the cup.
 setFixedTransform(body.Joint, trvec2tform(cupPosition))
 addBody(lbr, body, lbr.BaseName);
+
+%Define the Planning Problem
 numWaypoints = 5;
 q0 = homeConfiguration(lbr);
 qWaypoints = repmat(q0, numWaypoints, 1);
 gik = generalizedInverseKinematics('RigidBodyTree', lbr, ...
     'ConstraintInputs', {'cartesian','position','aiming','orientation','joint'})
 
-heightAboveTable = constraintCartesianBounds(gripper);
+%Create Constraint Objects
+heightAboveTable = constraintCartesianBounds(gripper); 
+%Create a Cartesian bounds constraint that requires the gripper to be at least 5 cm above the table (negative z direction)
 heightAboveTable.Bounds = [-inf, inf; ...
                            -inf, inf; ...
                            0.05, inf]
